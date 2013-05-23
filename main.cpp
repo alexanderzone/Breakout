06419a6eff962dc7e06bc94115641eb8526b03c6
Alexander Mistakidis <mistakidis@gmail.com>
2013-05-22 23:45:53 -0400

    continued event handling, basic paddle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "SDL.h"
 #include "SDL_opengl.h"
 #include <iostream>
+#include <math.h>  //fmod
 
 int main (int argc, char* args[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -41,11 +42,37 @@ int main (int argc, char* args[]) {
 
     std::cout << "OpenGL is running\n";
 
-    bool isRunning = true;
+    class Brick {
+       public:
+          double h = 20;
+          double w = 100;
+          double x = 0;
+          double y = 0;
+    };
 
-    //generic event var for handling events
+    Brick player;
+    player.x = 250;
+    player.y = 380;
+
+    bool isRunning = true;
+    //Generic event var for handling events
     SDL_Event event;
+
     while(isRunning) {
+
+        //Ghetto update frequency
+        SDL_Delay(20);
+
+    //Event handling
+        Uint8* keystate = SDL_GetKeyState(NULL);
+
+        //continuous-response keys
+        if(keystate[SDLK_LEFT]) {
+            player.x -= 5;
+        }
+        if(keystate[SDLK_RIGHT]) {
+             player.x += 5;
+        }
 
         //On event
         while (SDL_PollEvent(&event)) {
@@ -60,8 +87,11 @@ int main (int argc, char* args[]) {
                         case SDLK_ESCAPE:
                             isRunning = false;
                             break;
-                        case SDLK_r:
-                            glClearColor(1,0,0,1);
+                        case SDLK_LEFT:
+                            player.x -= 5;
+                            break;
+                        case SDLK_RIGHT:
+                            player.x += 5;
                             break;
                         default:
                             //unused key, do nothing
@@ -73,9 +103,33 @@ int main (int argc, char* args[]) {
             }
         }
 
+    //Game Logic
+        //wrap-around x axis
+        if(player.x < 0) {
+            player.x = fmod(player.x+600, 600);
+        }
+        player.x = fmod(player.x, 600);
+
+    //Render Logic
         //Clear the screen with set color
         glClear(GL_COLOR_BUFFER_BIT);
+        //Gives us the 'grid' we draw stuff on
+        glPushMatrix();
+        //Set the matrix size by multiplying an orthographic matrix
+        glOrtho(0,600,400,0,-1,1);
 
+        //Set the color of primitives
+        glColor4f(.5,1,1,1);
+
+        glBegin(GL_QUADS);//start drawing primitive
+        glVertex2f(player.x, player.y);
+        glVertex2f(player.x, player.y + player.h);
+        glVertex2f(player.x + player.w, player.y + player.h);
+        glVertex2f(player.x + player.w, player.y);
+        glEnd(); //end drawing
+
+        //Pops the matrix back to render on the screen
+        glPopMatrix();
         //Pushes the write buffer to the screen
         SDL_GL_SwapBuffers();
     }
